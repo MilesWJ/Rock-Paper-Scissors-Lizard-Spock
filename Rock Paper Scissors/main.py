520cf8e0a567f22814a6b210ae9f4319fe21bb82
Miles James <milesw.james@gmail.com>
2021-03-17 16:39:15 -0700

    Update main.py

--- Rock Paper Scissors/main.py
+++ Rock Paper Scissors/main.py
@@ -3,21 +3,17 @@ from player_computer import Player, Computer
 
 
 def computer_decide():
-
     Computer.random = random.randint(1, 3)
 
     if Computer.random == 1:
         Computer.move = "rock"
-
     elif Computer.random == 2:
         Computer.move = "paper"
-
     elif Computer.random == 3:
         Computer.move = "scissors"
 
 
 def play_game():
-
     Player.move = str(input(
         "\n[MOVES: ROCK, PAPER, SCISSORS]\nWhat move would you like to use? ")).lower()
 
@@ -27,11 +23,11 @@ def play_game():
         print("\nPlayer chose rock!")
         print("Computer chose rock!")
         print("It's a tie!")
-    if Player.move == "rock" and Computer.move == "paper":
+    elif Player.move == "rock" and Computer.move == "paper":
         print("\nPlayer chose rock!")
         print("Computer chose paper!")
         print("Computer wins!")
-    if Player.move == "rock" and Computer.move == "scissors":
+    else:
         print("\nPlayer chose rock!")
         print("Computer chose scissors!")
         print("Player wins!")
@@ -40,11 +36,11 @@ def play_game():
         print("\nPlayer chose paper!")
         print("Computer chose rock!")
         print("Player wins!")
-    if Player.move == "paper" and Computer.move == "paper":
+    elif Player.move == "paper" and Computer.move == "paper":
         print("\nPlayer chose paper!")
         print("Computer chose paper!")
         print("It's a tie!")
-    if Player.move == "paper" and Computer.move == "scissors":
+    else:
         print("\nPlayer chose paper!")
         print("Computer chose scissors!")
         print("Computer wins!")
@@ -53,11 +49,11 @@ def play_game():
         print("\nPlayer chose scissors!")
         print("Computer chose rock!")
         print("Computer wins!")
-    if Player.move == "scissors" and Computer.move == "paper":
+    elif Player.move == "scissors" and Computer.move == "paper":
         print("\nPlayer chose scissors!")
         print("Computer chose paper!")
         print("Player wins!")
-    if Player.move == "scissors" and Computer.move == "scissors":
+    else:
         print("\nUser chose scissors!")
         print("Computer chose scissors!")
         print("It's a tie!")
