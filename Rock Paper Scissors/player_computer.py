c908124ab748f9f8e64c0a489057cfbeabb559b6
Miles James <milesw.james@gmail.com>
2021-03-24 08:14:09 -0700

    Delete Rock Paper Scissors directory

--- Rock Paper Scissors/player_computer.py
+++
@@ -1,9 +0,0 @@
-class Player:
-    def __init__(self, move):
-        self.move = move
-
-
-class Computer:
-    def __init(self, random, move):
-        self.random = random
-        self.move = move
